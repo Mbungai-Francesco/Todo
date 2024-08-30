5b169b1f7d011b25901d9375e24b72b819b876e2
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2024-08-30 15:39:21 +0100

    frontend done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LucideAngularModule, Sun } from 'lucide-angular'
+import { LucideAngularModule, Sun } from 'lucide-angular';
+import { ButtonModule } from 'primeng/button';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LucideAngularModule],
+  imports: [RouterOutlet, LucideAngularModule, ButtonModule, ListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Todo';
-  readonly icons = { Sun }
+  list : string[] = []
+  readonly icons = { Sun };
+
+  onEnter = (event: KeyboardEvent) => {
+    if (event.key.toLocaleLowerCase() === 'enter') {
+      const inputElement = event.target as HTMLInputElement;
+      const newValue = inputElement.value.trim();
+      if (newValue) {
+        this.list.push(newValue);
+        inputElement.value = ''
+      }
+    }
+  };
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LucideAngularModule, Check } from 'lucide-angular'
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  constructor(){
+    console.log(this.checkClass);
+  }
+  readonly icons = { Check }
+  checked = false
+  @Input({required: true}) list?: string
+  checkClass = this.checked ? "w-6 aspect-square border rounded-full mr-4 border-myDarkBlue-300 flex justify-center items-center myCheck" : "w-6 aspect-square border rounded-full mr-4 border-myDarkBlue-300 flex justify-center items-center"
 
+  clickHandler = () => {
+    this.checked = !this.checked
+  }
 }
